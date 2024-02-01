dbfe5aba97c76fb73c0777e337dc20b552d2c8d0
Ronakmewara <mewararonak00@gmail.com>
2024-02-01 23:14:00 +0530

    search Functionality

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -24,10 +24,23 @@ class HomepageState extends State<Homepage> {
      return post.map((e) => Post.fromJson(e)).toList();
   }
 
+  final TextEditingController searchController = TextEditingController();
+   late List<Post> posts;
+   late List<Post> filteredPosts;
   @override
   void initState() {
     super.initState();
 
+      fetchData().then((data){
+        setState(() {
+          posts = data;
+          filteredPosts = data;
+        });
+      });
+  }
+
+  void onSearchTextChanged(String searchText){
+    filteredPosts = posts.where((post) => post.title!.toLowerCase().contains(searchText.toLowerCase())).toList();
   }
 
   @override
@@ -36,81 +49,100 @@ class HomepageState extends State<Homepage> {
      backgroundColor: buttonLightGreen,
 
       body: Center(
-        child: FutureBuilder(
-          future: fetchData(),
-           builder: (context , snapshot){
-            if(snapshot.connectionState == ConnectionState.done){
-              if(snapshot.hasError){
-                            return const Center(
-                              child: Text('No data Found'),
-                            );
-              } else if(snapshot.hasData){
-                return ListView.builder(
 
 
-                  itemCount: snapshot.data?.length,
-                  itemBuilder: (context, index) {
-                    final post = snapshot.data?[index];
-                    return Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Container(
 
-                        decoration: BoxDecoration(
+        child: Column(
+          children: [
+            TextField(
+              controller: searchController ,
+              onChanged: onSearchTextChanged,
+              decoration: const InputDecoration(
+                hintText: 'Search...',
+                prefixIcon: const Icon(Icons.search)
 
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: const [
-                              BoxShadow(
-                                  color: Colors.black,
-                                  offset: Offset(1, 1),
-                                  blurRadius: 2
+              ),
+            ),
+
+            Expanded(
+              child: FutureBuilder(
+                future: fetchData(),
+                 builder: (context , snapshot){
+                  if(snapshot.connectionState == ConnectionState.done){
+                    if(snapshot.hasError){
+                                  return const Center(
+                                    child: Text('No data Found'),
+                                  );
+                    } else if(snapshot.hasData){
+                      return ListView.builder(
+              
+              
+                        itemCount: filteredPosts.length,
+                        itemBuilder: (context, index) {
+                          final post = filteredPosts[index];
+                          return Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Container(
+              
+                              decoration: BoxDecoration(
+
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: const [
+                                    BoxShadow(
+                                        color: Colors.black,
+                                        offset: Offset(1, 1),
+                                        blurRadius: 2
+                                    ),
+                                  ]
+              
                               ),
-                            ]
-
-                        ),
-
-                        child: Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: ListTile(
-
-
-                            onTap: () {
-                              Navigator.of(context).pushReplacement(MaterialPageRoute(
-                                  builder: (context) =>
-                                      DetailsPage(post: post)));
-                            },
-
-                            leading: CircleAvatar(
-                                backgroundColor: buttonLightGreen,
-                                maxRadius: 40,
-                                child: Text(
-                                  post!.id.toString(),
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: primaryColor),
-                                )),
-                            title: Text(post.title!, style: Theme
-                                .of(context)
-                                .textTheme
-                                .titleLarge!
-                                .copyWith(fontSize: 17),),
-                            trailing: const Icon(Icons.arrow_forward_ios),
-                          ),
-                        ),
-                      ),
-
-                    );
-                  },
-                );
-              }
-
-            }
-            return const Center(
-              child: CircularProgressIndicator.adaptive(),
-            );
-
-           },
-    ),
+              
+                              child: Padding(
+                                padding: const EdgeInsets.all(10),
+                                child: ListTile(
+              
+              
+                                  onTap: () {
+                                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                                        builder: (context) =>
+                                            DetailsPage(post: post)));
+                                  },
+              
+                                  leading: CircleAvatar(
+                                      backgroundColor: buttonLightGreen,
+                                      maxRadius: 40,
+                                      child: Text(
+                                        post.id.toString(),
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: primaryColor),
+                                      )),
+                                  title: Text(post.title!, style: Theme
+                                      .of(context)
+                                      .textTheme
+                                      .titleLarge!
+                                      .copyWith(fontSize: 17),),
+                                  trailing: const Icon(Icons.arrow_forward_ios),
+                                ),
+                              ),
+                            ),
+              
+                          );
+                        },
+                      );
+                    }
+              
+                  }
+                  return const Center(
+                    child: CircularProgressIndicator.adaptive(),
+                  );
+              
+                 },
+                  ),
+            ),
+          ],
+        ),
       )
 
 
